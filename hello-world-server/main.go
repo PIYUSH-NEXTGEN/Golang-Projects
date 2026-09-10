07c132ce456e5ec413c23c23db56c3023a0835d3
Piyush Baraskar <piyushbaraskar4@gmail.com>
2026-09-11 02:48:19 +0530

    Created a  web server that responds with "Hello, there".

---
+++ hello-world-server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, there")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Server running on http://localhost:8080")
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
